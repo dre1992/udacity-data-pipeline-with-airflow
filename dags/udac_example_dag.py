3374d0347c3185e066ab604df565c58a10ad7785
Andreas Seficha <aseficha@gmail.com>
2020-12-15 17:53:54 +0100

    Update udac_example_dag.py

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -31,7 +31,7 @@ args = {
 }
 
 dag = DAG(
-    dag_id='example_bash_operator',
+    dag_id='example_bash_git',
     default_args=args,
     schedule_interval='0 0 * * *',
     dagrun_timeout=timedelta(minutes=60),
